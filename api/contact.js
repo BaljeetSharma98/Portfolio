731aeac6c4bcced2a0d97eaafe119685a5a90af2
Baljeet <baljeetsharma.abc@gmail.com>
2026-07-29 16:23:32 +0530

    contact

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,13 @@
 import { MongoClient } from 'mongodb';
 import { Resend } from 'resend';
 
+
+
+console.log("Mongo URI:", process.env.MONGODB_URI);
+console.log("Resend:", process.env.RESEND_API_KEY);
+
+
+
 const client = new MongoClient(process.env.MONGODB_URI);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
